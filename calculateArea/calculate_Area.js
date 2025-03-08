ac8d06068023ca10580aeb2bb7984dbd81b035f6
xlongx <xlongx@live.com>
2025-03-07 22:43:50 -0500

    Function added

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -2,9 +2,8 @@ let length;
 let width;
 
 function calculateArea() {
- length = parseFloat(document.getElementById('length').value);
- width = parseFloat(document.getElementById('width').value);
- let area = length * width;
+    length = parseFloat(document.getElementById('length').value);
+    width = parseFloat(document.getElementById('width').value);
+    let area = length * width;
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
-
- document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
